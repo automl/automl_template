54bf0702da6fa5f6e70090f8f01fa5c72dcedf2d
eddiebergman <eddiebergmanhs@gmail.com>
2022-02-15 12:31:25 +0100

    clean me

---
+++ _templates/docs/conf.py
@@ -0,0 +1,16 @@
+import automl_sphinx_theme
+
+from <<package-name>> import copyright, author, version, name
+
+
+options = {
+    "copyright": copyright,
+    "author": author,
+    "version": version,
+    "name": name,
+    "html_theme_options": {
+        "github_url": "https://automl.github.io/<<name>>/main"
+    }
+}
+
+automl_sphinx_theme.set_options(globals(), options)
--- tests/conftest.py
+++
@@ -1,1 +0,0 @@
-# For configuring pytest
