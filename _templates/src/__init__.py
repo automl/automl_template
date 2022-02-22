50b883bf9819186e2105e8c5d14710a78f5771e3
eddiebergman <eddiebergmanhs@gmail.com>
2022-02-22 19:35:39 +0100

    Fix: missing import if not packeging

--- _templates/src/__init__.py
+++ _templates/src/__init__.py
@@ -7,11 +7,9 @@ author = "<<author>>"
 author_email = "<<email>>"
 description = "<<description>>"
 url = "<<url>>"
-<<requires::packaging
 project_urls = {
     <<requires::docs "Documentation": "https://automl.github.io/<<package-name>>/main" endrequires::docs>>,
     "Source Code": "https://github.com/automl/<<package-name>>",
 }
-endrequires::packaging>>
 copyright = f"Copyright {datetime.date.today().strftime('%Y')}, AutoML.org Freiburg-Hannover"
 version = "0.0.1"
