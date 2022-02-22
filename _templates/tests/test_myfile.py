05f7d52503ae2b47626dd53db1b3673e883c1b65
eddiebergman <eddiebergmanhs@gmail.com>
2022-02-22 18:58:32 +0100

    Fix: tests

--- _templates/tests/test_myfile.py
+++ _templates/tests/test_myfile.py
@@ -7,7 +7,7 @@ def test_oreos():
     """
     Should add `a` and the param `x`
     """
-    myclass = MyClasses(a=3)
+    myclass = MyClass(a=3, b={})
     assert myclass.oreos(2) == 5
 
 
@@ -17,6 +17,6 @@ def test_construction_with_negative_a_raises_error(value):
     Should raise a ValueError with a negative `a`
     """
     with pytest.raises(ValueError):
-        MyClass(a=value)
+        MyClass(a=value, b={})
 
 
